f9370ac573fcc544a79370ed1aecbc6ce4cf0ca8
Caleb Sherfick <calebsherfick@gmail.com>
2019-03-08 16:09:52 -0700

    Added table for bug results

--- src/components/Bugs.vue
+++ src/components/Bugs.vue
@@ -1,10 +1,34 @@
 <template>
   <div class="bugs col-12 d-flex justify-content-center mt-5">
-    <ul v-for="bug in allBugs" :key="bug.id">
-      <li @click="setActiveBug(bug)">
+    <ul v-for="bug in allBugs" :key="bug.id" class="list-group d-flex flex-column row">
+      <li @click="setActiveBug(bug)" class="list-group-item offset-2 col-md-10">
         <router-link :to="{name: 'details', params:{id: bug._id}}">{{bug.title}}</router-link>
       </li>
     </ul>
+  </div>
+</template>
+
+
+<template>
+  <div class="bugs col-12 d-flex justify-content-center mt-5">
+    <table class="table">
+      <thead>
+        <tr>
+          <th>Title</th>
+          <th>Reported By</th>
+          <th>Status</th>
+          <th>Date</th>
+        </tr>
+      </thead>
+      <tbody v-for="bug in allBugs" :key="bug.id">
+        <tr>
+          <td>{{bug.title}}</td>
+          <td>{{bug.creator}}</td>
+          <td>{{bug.closed}}</td>
+          <td>{{bug.createdAt}}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
@@ -33,5 +57,6 @@ export default {
 li {
   list-style: none;
   cursor: pointer;
+  color: black;
 }
 </style>
